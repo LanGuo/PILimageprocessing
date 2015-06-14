1125ecb38002c57ee23c59f95699cfe1f496d526
languo <lan.guo14@gmail.com>
2015-06-13 23:14:03 -0700

    First commit to piltest, with README file and basic frame work.

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is a test to use Python Imaging Library to work with images.
---
+++ piltest.py
@@ -0,0 +1,20 @@
+'''
+This is a python program using Python Imaging Library to manipulate image files.
+'''
+
+# Interact with user to get file path, channel names
+
+# Isolate file names in that directory
+
+# Make n lists of filenames (n=number of channels)
+
+# Zip the lists to make a merge list; items in this list are tuples of the n channels of one section
+
+# Use image.merge to merge the channels and get a composite image
+
+# Output and save image
+
+# Rotate image using registration
+
+
+  
