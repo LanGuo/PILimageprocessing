912caad71ff25a0223826ada361105be3959c612
LanGuo <lan.guo14@gmail.com>
2015-06-15 11:51:02 -0700

    minor edits, need to figure out how to save merged images with correct names

--- piltest.py
+++ piltest.py
@@ -5,35 +5,44 @@ This is a python program using Python Imaging Library to manipulate image files.
 import os
 from os import listdir
 from os.path import isfile, join
+from PIL import Image
 
-# Interact with user to get file path, channel names
+
 def getImagingFiles(inputdir):
     """
-    Get filenames of images in the input directory.
+    Get filenames of images in the user specified input directory.
     """
     if not os.path.isdir(inputdir):
         raise RuntimeError, "The directory provided does not exist"
     # Get all the filenames from the input directory, right now ignoring subdirectories. May consider using os.walk to get files from subdirectories in the future.
     onlyfiles = [ f for f in listdir(inputdir) if isfile(join(inputdir,f)) ]
-    onlyfilespull = join(inputdir, onlyfiles)
+    onlyfilesfull = join(inputdir, onlyfiles)
     return onlyfilesfull
 
-# Make n lists of filenames (n=number of channels), should call it with onlyfilesfull and channelnames.  
+  
 def getChannels(filenames, channelnames):
+    """
+    Based on user specified channel names, generate separate lists of filenames for that channel, stored in a dictionary.
+    """
     # num_channels = len(channelnames)
     for c in channelnames: 
         channelmap = {c : sorted(f for f in filenames if f.endswith(c))}
     return channelmap
 
-# Use image.merge to iterate over the channel lists and merge the channels of the same section to get a composite image
+
 def mergeAllChannels(channelmap):
-    sectionstomerge = map(channelmap.values()) #this should return a list of tuples with the different channels from the same section
+    """
+    Use image.merge to iterate over the channel lists and merge the channels of the same section to get a composite image
+    """
+    # Mapping the lists of filenames in channelmap to generate a list of tuples with the different channels from the same section
+    sectionstomerge = map(channelmap.values()) 
     for s in sectionstomerge:    
         mergedfile = Image.merge("RGB", s)
+        mergedfile.save()
 
-# Output and save image
 
-# Rotate image using registration
+
+# Image rotation and registration
 
 
   
